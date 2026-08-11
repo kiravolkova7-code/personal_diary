012c85187a2dfaf79010240dee8324b5013ae855
august <kira_volkova_7@mail.ru>
2026-08-12 02:02:02 +0500

    path

--- config/settings.py
+++ config/settings.py
@@ -108,7 +108,7 @@ LOCALE_PATHS = [
 ]
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = "/vol/web/media/"
