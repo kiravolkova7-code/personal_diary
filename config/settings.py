c4d0e835ce89d3e88954b34ce32fcacc67353c1f
august <kira_volkova_7@mail.ru>
2026-08-11 16:19:17 +0500

    delete staticfile_dirs

--- config/settings.py
+++ config/settings.py
@@ -108,9 +108,6 @@ LOCALE_PATHS = [
 
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "static"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = "/vol/web/media/"
