e42a01cada6755ffc365ae59aebe813a063c5fdd
august <kira_volkova_7@mail.ru>
2026-08-12 23:55:22 +0500

    path

--- config/settings.py
+++ config/settings.py
@@ -138,7 +138,7 @@ CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
 ]
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", "redis://redis:6379/0")
